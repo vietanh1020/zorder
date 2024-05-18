8d7f92be56114fb37f820625d3ccebd784f75127
AnhVV <anh.vv@zinza.com.vn>
2024-05-18 19:57:46 +0700

    update code

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
-import { CronjobService } from './cronjob/cronjob.service';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,7 +15,7 @@ async function bootstrap() {
   );
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'http://zorder.site',
     credentials: true,
   });
 
